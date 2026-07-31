1faa95a974b0ec10d595f2cdabf718fabe777c46
Abdülhay Şahin <192990669+sahinsama@users.noreply.github.com.>
2026-07-31 16:38:00 +0300

    Backend successfully deployed to Render

--- lexvault-web/src/App.jsx
+++ lexvault-web/src/App.jsx
@@ -1,47 +1,49 @@
-import { useState } from 'react'
-import './App.css'
-import WordCard from './components/WordCard'
-
+import { useState } from "react";
+import "./App.css";
+import WordCard from "./components/WordCard";
 
 function App() {
-
-  const [word, setWord] = useState("")
-  const [result, setResult] = useState(null)
+  const [word, setWord] = useState("");
   const [source, setSource] = useState("");
+  const [result, setResult] = useState(null);
 
   async function handleAnalyze() {
-
-    if (!word.trim()) return
+    if (!word.trim()) return;
 
     const response = await fetch(
-      "http://localhost:3000/analyze",
+      "https://lexvault-48l2.onrender.com/analyze",
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-        word: word,
-        source: source,
-})
+          word,
+          source,
+        }),
       }
-    )
+    );
 
-    const data = await response.json()
-
-    setResult(data)
-
+    const data = await response.json();
+    setResult(data);
   }
-
 
   return (
     <>
       <h1>LexVault</h1>
 
       <input
+        type="text"
+        placeholder="Enter a word..."
         value={word}
         onChange={(e) => setWord(e.target.value)}
-        placeholder="Enter a word..."
+      />
+
+      <input
+        type="text"
+        placeholder="Source (optional)"
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
       />
 
       <button onClick={handleAnalyze}>
@@ -49,28 +51,13 @@ function App() {
       </button>
 
       {result && (
-        <WordCard 
+        <WordCard
           word={word}
           result={result}
         />
       )}
-
-      <input
-  type="text"
-  placeholder="Kelime"
-  value={word}
-  onChange={(e) => setWord(e.target.value)}
-/>
-
-<input
-  type="text"
-  placeholder="Kaynak (opsiyonel)"
-  value={source}
-  onChange={(e) => setSource(e.target.value)}
-/>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
