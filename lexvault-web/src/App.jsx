6fcb213a165126f0d09fdff1fb7714548f0cd950
Abdülhay Şahin <192990669+sahinsama@users.noreply.github.com.>
2026-08-06 12:46:22 +0300

    deploy after file changes

--- lexvault-web/src/App.jsx
+++ lexvault-web/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./app.css";
 import Home from "./pages/home.jsx";
 import Vault from "./pages/vault.jsx";
 
--- lexvault-web/src/pages/home.jsx
+++ lexvault-web/src/pages/home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./Home.css";
+import "./home.css";
 
 function Home() {
   return (
